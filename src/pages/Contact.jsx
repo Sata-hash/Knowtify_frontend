2b7368f256bf8ccaa3ce49a75e0782591bfe472f
Satavisa Das <142712421+Sata-hash@users.noreply.github.com>
2024-10-15 16:41:33 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+/*import React from "react"
 
 import Footer from "../components/common/Footer"
 import ContactDetails from "../components/ContactPage/ContactDetails"
@@ -9,18 +9,18 @@ const Contact = () => {
   return (
     <div>
       <div className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row">
-        {/* Contact Details */}
+        {/* Contact Details 
         <div className="lg:w-[40%]">
           <ContactDetails />
         </div>
 
-        {/* Contact Form */}
+        {/* Contact Form 
         <div className="lg:w-[60%]">
           <ContactForm />
         </div>
       </div>
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-        {/* Reviws from Other Learner */}
+        {/* Reviws from Other Learner 
         <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
         </h1>
@@ -31,4 +31,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact */
