e1ded4c56ad015d15744ed4728acf24baa13459d
Satavisa Das <142712421+Sata-hash@users.noreply.github.com>
2024-10-15 16:41:02 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+/*import React from "react"
 
 import FoundingStory from "../assets/Images/FoundingStory.png"
 import BannerImage1 from "../assets/Images/aboutus1.webp"
@@ -110,17 +110,17 @@ const About = () => {
       </section>
 
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-        {/* Reviws from Other Learner */}
+        {/* Reviws from Other Learner 
         <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
         </h1>
-        {/* <ReviewSlider /> */}
+        {/* <ReviewSlider /> 
         <ReviewSlider />
       </div>
-      {/* <Footer /> */}
+      {/* <Footer /> 
       <Footer />
     </div>
   )
 }
 
-export default About
+export default About */
